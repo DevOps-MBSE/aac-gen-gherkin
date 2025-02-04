048910553263aa811402718dd18f8dd688fef063
william Bunch <william.bunch@ngc.com>
2025-02-04 20:25:49 +0000

    simplified generation of dictionary files

--- python/src/gen_gherkin/generate_dictionary_files_impl.py
+++ python/src/gen_gherkin/generate_dictionary_files_impl.py
@@ -1,5 +1,4 @@
 """The implementation module for the gen-dictionary-helpers command in the AaC Generate Gherkin Feature Files plugin."""
-import yaml
 import json
 from os import path, makedirs
 from typing import Callable
@@ -11,8 +10,6 @@ from aac.execute.aac_execution_result import (
     MessageLevel,
 )
 from aac.in_out.parser._parse_source import parse
-
-from .gen_dictionary_file_helpers import get_template_properties
 
 plugin_name = "Generate Gherkin Feature Files"
 
@@ -46,38 +43,23 @@ def gen_dictionary_file(architecture_file: str, output_directory: str) -> tuple[
     messages: list[ExecutionMessage] = []
 
     dictionary_steps = parse(architecture_file)
-    # for d in dictionary_steps:
-    #     print(d.structure)
-    # results = get_template_properties(definitions_dictionary)
-    # yaml_list = []
-    # for model in results:
-    #     for acceptance in model["acceptance"]:
-    #         yaml_list.append([{"acceptance": acceptance}])
-
-    # new_file = ""
-    # for yaml_object in yaml_list:
-    #     new_file = new_file + yaml.safe_dump_all(yaml_object, default_flow_style=False, sort_keys=False, explicit_start=True)
-    # dictionary_steps = definitions_dictionary.structure
     files = {}
+
     for dictionary_step_definition in dictionary_steps:
-        # print(dictionary_step_definition)
-        dictionary_step = dictionary_step_definition.structure["dictionary_step"]
-        # print(dictionary_step)
-        if dictionary_step["feature_name"] in files.keys():
-            files[dictionary_step["feature_name"]][dictionary_step["name"]] = {
-                "statements": dictionary_step["statements"],
-                "functions": dictionary_step["functions"]
-            }
-            # print(files[dictionary_step["feature_name"]])
-        else:
-            files[dictionary_step["feature_name"]] = {
-                dictionary_step["name"]: {
+        if "dictionary_step" in dictionary_step_definition.content:
+            dictionary_step = dictionary_step_definition.structure["dictionary_step"]
+            if dictionary_step["feature_name"] in files.keys():
+                files[dictionary_step["feature_name"]][dictionary_step["name"]] = {
                     "statements": dictionary_step["statements"],
                     "functions": dictionary_step["functions"]
                 }
-            }
-            # print(files[dictionary_step["feature_name"]])
-    # print(files)
+            else:
+                files[dictionary_step["feature_name"]] = {
+                    dictionary_step["name"]: {
+                        "statements": dictionary_step["statements"],
+                        "functions": dictionary_step["functions"]
+                    }
+                }
 
     if len(files.keys()) < 1:
         msg = ExecutionMessage(
@@ -109,7 +91,6 @@ def after_gen_dictionary_file(architecture_file: str, output_directory: str) -> 
     files, execution_status = gen_dictionary_file(architecture_file, output_directory)
     for key in files.keys():
 
-            # print("======")
         filepath = f"{output_directory}/{key}.json"
         filepath = "_".join(filepath.split())
         makedirs(path.dirname(filepath), exist_ok=True)
